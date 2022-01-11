93a67507593d5d28e4a82f0a1611221135ebc8e2
monsteroff <cahangir.qanizade@gmail.com>
2022-01-11 19:13:45 +0400

    React 1 hw upload redo

--- REACT/REACT_01/homework/src/App.js
+++ REACT/REACT_01/homework/src/App.js
@@ -1,31 +1,14 @@
-import {useState, useEffect } from "react"
-
+import Users from "./components/Users";
+import Posts from "./components/Posts";
+import Comments from "./components/Comments";
 import "./App.css"
 
-import User from "./components/User"
-import Post from "./components/Post"
-import Comment from "./components/Comment"
-
 function App() {
-  const [users, setUsers] = useState({})
-  const [posts, setPosts] = useState({})
-  const [comments, setComments] = useState({})
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(val => setUsers(val))
-    fetch("https://jsonplaceholder.typicode.com/posts").then(res => res.json()).then(val => setPosts(val))
-    fetch("https://jsonplaceholder.typicode.com/comments").then(res => res.json()).then(val => setComments(val))
-  }, [])
   return (
     <div className="App">
-      <div className="users">
-        {users.length? users.map(user => <User key={user.id} user={user}/>) : false}
-      </div>
-      <div className="posts">
-        {posts.length? posts.map(post => <Post key={post.id} post={post}/>) : false}
-      </div>
-      <div className="comments">
-        {comments.length? comments.map(comment => <Comment key={comment.id} comment={comment} />) : false}
-      </div>
+        <Users />
+        <Posts />
+        <Comments />
     </div>
   );
 }
